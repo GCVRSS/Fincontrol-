f1c099c5cb0c60a86fa8f3d8f4d8fef8962ae575
Guilherme Cavarsan <guilhermecavarsan@alunos.fho.edu.br>
2026-08-15 19:41:22 -0300

    fix: adiciona STATIC_ROOT e STORAGES para configuracao correta do whitenoise

--- fincontrol-backend/core/settings.py
+++ fincontrol-backend/core/settings.py
@@ -125,7 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 CORS_ALLOW_ALL_ORIGINS = True  # depois trocamos isso por algo mais restritocd 
 
